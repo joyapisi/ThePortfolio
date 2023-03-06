4c91638c2f391169361ea5147c75452c634ec003
Joy Phoebe <38925103+joyapisi@users.noreply.github.com>
2023-03-06 11:13:15 -0800

    fix eslint errors

--- index.js
+++ index.js
@@ -1,11 +1,45 @@
-/* eslint-disable no-unused-vars */
-function menuPopup(){
-    document.getElementById("navigationList").classList.toggle("active");
-    document.getElementById("nav-button2").classList.toggle("active");
-    document.getElementById("closeButton").classList.toggle("active");
-  
-    if(window.innerWidth<768){
-      document.getElementById("navigationList").classList.toggle("active");
-      return false;
-    }
+// const NavList = document.getElementById('navigationList');
+
+// function menuPopup() {
+//   navList.classList.toggle('active');
+//   document.getElementById('nav-button2').classList.toggle('active');
+//   document.getElementById('closeButton').classList.toggle('active');
+
+//   if (window.innerWidth > 768) {
+//     document.getElementById('navigationList').classList.toggle('active');
+//     return false;
+//   }
+//   return true;
+// }
+// NavList.addEventListener('click', menuPopup);
+
+const NavList = document.getElementById('navigationList');
+const Hamburger = document.getElementById('nav-button2');
+const CloseButton = document.getElementById('closeButton');
+
+function menuPopup() {
+  NavList.classList.toggle('active');
+  Hamburger.classList.toggle('active');
+  CloseButton.classList.toggle('active');
+
+  if (window.innerWidth > 768) {
+    document.getElementById('navigationList').classList.toggle('active');
+    return false;
   }
+  return true;
+}
+NavList.addEventListener('click', menuPopup);
+
+// const navList = document.getElementById('navigationList');
+// function menuPopup() {
+//   navList.classList.toggle('active');
+
+//   document.getElementById('nav-button2').classList.toggle('active');
+//   document.getElementById('closeButton').classList.toggle('active');
+
+//   if (window.innerWidth < 768) {
+//     document.getElementById('navigationList').classList.toggle('active');
+//     return false;
+//   }
+// }
+// navList.addEventListener('click', menuPopup);
