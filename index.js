b9d632e2164109597eff106c08c9d23b03ae977a
Joy Phoebe <38925103+joyapisi@users.noreply.github.com>
2023-03-06 07:07:48 -0800

    fix linter errors

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
+/* eslint-disable no-unused-vars */
 function menuPopup(){
     document.getElementById("navigationList").classList.toggle("active");
     document.getElementById("nav-button2").classList.toggle("active");
     document.getElementById("closeButton").classList.toggle("active");
-  }
-  function menuPopup(){
+  
     if(window.innerWidth<768){
       document.getElementById("navigationList").classList.toggle("active");
       return false;
