d3e14dc4346b27d956289d6eb1b1eb72217dda3d
Dave <davidolanrewaju538@gmail.com>
2023-03-06 13:48:55 +0100

    JS file update

--- index.js
+++ index.js
@@ -1,2 +1,5 @@
-
-function menuPopup(){}
+function menuPopup(){
+    document.getElementById("navigationList").classList.toggle("active");
+    document.getElementById("nav-button2").classList.toggle("active");
+    document.getElementById("closeButton").classList.toggle("active");
+  }
