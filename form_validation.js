bf783b01e78316987d34628ff331502e6c387cc0
Olanrewaju David <63909901+dave-prog@users.noreply.github.com>
2023-03-11 10:59:08 +0100

    Remove commented body of code from codebase

--- form_validation.js
+++ form_validation.js
@@ -2,23 +2,6 @@ const email = document.getElementById('E_mail');
 const error = document.getElementById('Feed_back');
 const form = document.getElementById('Form');
 
-// const emailValidation = (input) => {
-//   if (input === input.toLowerCase()) {
-//     return true;
-//   }
-//   return false;
-// };
-
-// form.addEventListener('submit', (e) => {
-//   error.innerHTML = '';
-//   if (emailValidation(email.value)) {
-//     error.innerHTML = '';
-//   } else {
-//     e.preventDefault();
-//     error.innerHTML = 'Oh dear! Please Change Your e_mail to lower case! ☺️☺️';
-//   }
-// });
-
 form.addEventListener('submit', (e) => {
     if (email.value !== email.value.toLowerCase()) {
       e.preventDefault();
