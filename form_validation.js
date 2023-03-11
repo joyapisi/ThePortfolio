4b8e39c72a6eb25cfffe1f61d05e9ff93c67fb60
Joy Phoebe <38925103+joyapisi@users.noreply.github.com>
2023-03-11 13:46:19 +0300

    fix eslinter errors

--- form_validation.js
+++ form_validation.js
@@ -3,9 +3,9 @@ const error = document.getElementById('Feed_back');
 const form = document.getElementById('Form');
 
 form.addEventListener('submit', (e) => {
-    if (email.value !== email.value.toLowerCase()) {
-      e.preventDefault();
-      error.id = 'Feed_back';
-      error.textContent = 'Oh dear! Please Change Your e_mail to lower case! ☺️☺️';
-    }
- });
+  if (email.value !== email.value.toLowerCase()) {
+    e.preventDefault();
+    error.id = 'Feed_back';
+    error.textContent = 'Oh dear! Please Change Your e_mail to lower case! ☺️☺️';
+  }
+});
