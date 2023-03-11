8f90d271186f5e08a00b90ec2ba3a2873bc39a78
Joy Phoebe <38925103+joyapisi@users.noreply.github.com>
2023-03-11 03:21:19 +0300

    fix linter errors

---
+++ local_storage.js
@@ -0,0 +1,20 @@
+const localData = document.querySelectorAll('.inputs');
+const localStoreData = {
+  name: '',
+  email: '',
+  message: '',
+};
+localData.forEach((input) => {
+  input.addEventListener('input', () => {
+    localStoreData[input.name] = input.value;
+    localStoreData[input.email] = input.value;
+    localStoreData[input.message] = input.value;
+    localStorage.setItem('information', JSON.stringify(localStoreData));
+  });
+});
+const informationStored = JSON.parse(localStorage.getItem('information'));
+if (informationStored) {
+  localData.forEach((element) => {
+    element.value = informationStored[element.name];
+  });
+}
