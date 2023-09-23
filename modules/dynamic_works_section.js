ca74173e06cfbc7f23b0052152da952906582969
Joy Phoebe <joyapisi@gmail.com>
2023-09-23 18:07:56 +0300

    Correct github links by removing dead code

--- modules/dynamic_works_section.js
+++ modules/dynamic_works_section.js
@@ -5,8 +5,8 @@ const worksCards = [{
   jobPosition: ['Self-Employed', 'Web Dev', '2023'],
   jobDescription: 'A web application for a company that provides commercial and scientific space travel services. Users can book rockets and join selected space missions.',
   TechStack: ['ReactJS', 'Redux', 'Javascript', 'HTML', 'CSS', 'Github'],
-  liveDemoButton: 'https://github.com/joyapisi/space-traveler-hub',
-  sourceLinkButton: 'https://my-space-travellers-hub.netlify.app/',
+  liveDemoButton: 'https://my-space-travellers-hub.netlify.app/',
+  sourceLinkButton: 'https://github.com/joyapisi/space-traveler-hub',
 },
 {
   id: 2,
@@ -15,8 +15,8 @@ const worksCards = [{
   jobPosition: ['Self-Employed', 'Web Dev', '2023'],
   jobDescription: 'A website for an annual teenager\'s conference where teen creators can learn how to create content and produce it using Adobe products.',
   TechStack: ['Javascript', 'HTML', 'CSS'],
-  liveDemoButton: 'joyapisi.github.io/1st-Capstone-Project/',
-  sourceLinkButton: 'https://joyapisi.github.io/1st-Capstone-Project/',
+  liveDemoButton: 'https://joyapisi.github.io/1st-Capstone-Project/',
+  sourceLinkButton: 'https://github.com/joyapisi/1st-Capstone-Project',
 },
 {
   id: 3,
@@ -25,8 +25,8 @@ const worksCards = [{
   jobPosition: ['Self-Employed', 'Web Dev', '2023'],
   jobDescription: 'This is a mobile app that can be used to learn more about various Artwork by clicking on its displayed name. All data is fetched from the Art Institute of Chicago API.',
   TechStack: ['Javascript', 'HTML', 'CSS'],
-  liveDemoButton: 'https://github.com/joyapisi/artwork-app',
-  sourceLinkButton: 'https://joys-artwork-app.netlify.app/',
+  liveDemoButton: 'https://joys-artwork-app.netlify.app/',
+  sourceLinkButton: 'https://github.com/joyapisi/artwork-app',
 },
 {
   id: 4,
@@ -35,8 +35,8 @@ const worksCards = [{
   jobPosition: ['Self-Employed', 'Web Dev', '2022'],
   jobDescription: 'A mobile app that can be used to find details of various artwork displayed on the home page. All data is fetched from the Art Institute of Chicago API.',
   TechStack: ['Rails', 'PostgreSQL', 'ReactJS', 'Redux', 'Javascript', 'Github'],
-  liveDemoButton: 'https://github.com/joyapisi/budget-app',
-  sourceLinkButton: 'https://my-lifestyle-budget-app.onrender.com/',
+  liveDemoButton: 'https://my-lifestyle-budget-app.onrender.com/',
+  sourceLinkButton: 'https://github.com/joyapisi/budget-app/',
 }];
 
 function generateWorksCards(
@@ -46,7 +46,6 @@ function generateWorksCards(
   jobPosition,
   jobDescription,
   TechStack,
-  seeProject,
   liveDemoButton,
   sourceLinkButton,
 ) {
@@ -67,31 +66,30 @@ function generateWorksCards(
    <p class="paragraph2">${jobDescription}</p>
  
    <ul class="resources1" title="resources">
-    <li><a  class="button" href="#">${TechStack[0]}</a> <li>
-    <li><a  class="button" href="#">${TechStack[1]}</a> </li>
-    <li><a  class="button" href="#">${TechStack[2]}</a></li>      
-  </ul>
+     <li><a  class="button" href="#">${TechStack[0]}</a> <li>
+     <li><a  class="button" href="#">${TechStack[1]}</a> </li>
+     <li><a  class="button" href="#">${TechStack[2]}</a></li>      
+   </ul>
   
   <div class="popupaction">
-  <a href = "${liveDemoButton}" target="_blank">
-    <button class="popupseeLiveButton">
-      <div class="btnwrap"> 
-        See Live 
-        <img src="./images/popupaction.png" alt="see image live" />
-      </div>
-    </button>
-  </a> 
-  
-  <a href="${sourceLinkButton}" target="_blank">
-    <button class="popupseeSourceButton" >
-      <div class="btnwrap">
-        See Source
-        <img src="./images/popupgithub.png" alt="github source" />
-      </div>
-    </button> 
-  </a>
-
-</div>
+    <a href="${liveDemoButton}" target="_blank">
+      <button class="popupseeLiveButton">
+        <div class="btnwrap"> 
+          See Live 
+          <img src="./images/popupaction.png" alt="see image live" />
+        </div>
+      </button>
+    </a> 
+    
+    <a href="${sourceLinkButton}" target="_blank">
+      <button class="popupseeSourceButton">
+        <div class="btnwrap"> 
+          See Source 
+          <img src="./images/popupgithub.png" alt="github source" />
+        </div>
+      </button>
+    </a>
+  </div>
  
   </div>
 </div>
